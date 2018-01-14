53e50ced09d671599163b27a3f7ad7d73998b285
Arun Kothari <arun.kothari17@imperial.ac.uk>
2018-01-14 13:20:03 +0000

    added validation params

--- vgg_net.py
+++ vgg_net.py
@@ -35,21 +35,30 @@ class NN(object):
         sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
         self.model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
-    def train(self,directory_,model_name):
-        train_datagen = ImageDataGenerator(
+    def train(self,train_directory_,validation_directory_,model_name):
+        datagen = ImageDataGenerator(
             rescale=1. / 255,
             shear_range=0.2,
             zoom_range=0.2,
             horizontal_flip=True)
 
-        test_datagen = ImageDataGenerator(rescale=1. / 255)
+        datagen = ImageDataGenerator(rescale=1. / 255)
 
-        train_generator = train_datagen.flow_from_directory(
-            directory_,
+        train_generator = datagen.flow_from_directory(
+            train_directory_,
             target_size=(100, 100),
             batch_size=32,
             class_mode="categorical")  # CHANGE THIS!!!
-        self.model.fit_generator(train_generator, epochs=10)
+
+
+        validate_generator = datagen.flow_from_directory(
+            validation_directory_,
+            target_size=(100, 100),
+            batch_size=32,
+            class_mode="categorical")  # CHANGE THIS!!!
+
+
+        self.model.fit_generator(generator=train_generator, validation_data=validate_generator, epochs=10)
 
         current_directory = os.path.dirname(os.path.abspath(__file__))
         self.model.save(os.path.join(current_directory, os.path.pardir, "models", model_name))
