Fixed incorrect scaling of axes in GANN

GANN-v2_RGB_mygo.py
@@ -1,9 +1,9 @@
 import os
 import shutil
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 #matplotlib inline
 #config InlineBackend.figure_format = 'retina'
-from data_generator_bandw import *
+from data_generator import *
 import keras.backend as K
 from keras.datasets import mnist
 from keras.layers import *
@@ -29,15 +29,15 @@ class WGANN(object):
         self.Z_SIZE = IM_WIDTH
         # number of iterations D is trained for per each G iteration
         self.D_ITERS = 5
-        self.BATCH_SIZE = 5
+        self.BATCH_SIZE = 50
         self.ITERATIONS = 25000
-        self.NO_CHANNELS = 1
+        self.NO_CHANNELS = 3
         self.DG_losses = []
         self.D_true_losses = []
         self.D_fake_losses = []
 
         # write tensorboard summaries
-        self.sw = tf.summary.FileWriter(self.TENSORBOARD_DIR)
+        #self.sw = tf.summary.FileWriter(self.TENSORBOARD_DIR)
 
         # save 10x10 sample of generated images
         self.samples_zz = np.random.normal(0., 1., (100, self.Z_SIZE))
@@ -230,12 +230,12 @@ class WGANN(object):
         # generated image
         if sample_images:
             img = self.generate_samples(step, save=save_image_files)
-            s.MergeFromString(tf.Session().run(
-                tf.summary.image('samples_%07d' % step,
-                                 img.reshape([1, *img.shape]))))
+            #s.MergeFromString(tf.Session().run(
+                #tf.summary.image('samples_%07d' % step,
+                                 #img.reshape([1, *img.shape]))))
 
-        self.sw.add_summary(s, step)
-        self.sw.flush()
+        #self.sw.add_summary(s, step)
+        #self.sw.flush()
 
 
         # fake = 1
@@ -243,11 +243,11 @@ class WGANN(object):
 
     def train(self):
 
-        '''
+        
         params = {'dir': 'training_data', 'batch_size': self.BATCH_SIZE,'shuffle': True}
         train_data_gen = DataGenerator(**params).generate()
         params2 = {'dir': 'validation_data', 'batch_size': self.BATCH_SIZE, 'shuffle': True}
-        validation_data_gen = DataGenerator(**params).generate()'''
+        validation_data_gen = DataGenerator(**params).generate()
 
         progress_bar = Progbar(target=self.ITERATIONS)
 
@@ -259,17 +259,17 @@ class WGANN(object):
         for it in range(self.ITERATIONS):
 
             # load mnist data
-            #(X_train, y_train) = train_data_gen.__next__()
-            #(X_test, y_test) = validation_data_gen.__next__()
-            (X_train, y_train), (X_test, y_test) = mnist.load_data()
+            (X_train, y_train) = train_data_gen.__next__()
+            (X_test, y_test) = validation_data_gen.__next__()
+            #(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
             # use all available 70k samples
-            X_train = np.concatenate((X_train, X_test))
-            y_train = np.concatenate((y_train, y_test))
+            #X_train = np.concatenate((X_train, X_test))
+            #y_train = np.concatenate((y_train, y_test))
 
             # convert to -1..1 range, reshape to (sample_i, 28, 28, 1)
-            X_train = (X_train.astype(np.float32) - 127.5) / 127.5
-            X_train = np.expand_dims(X_train, axis=3)
+            #X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+            #X_train = np.expand_dims(X_train, axis=3)
             #print(self.D_true_losses)
 
 
@@ -316,18 +316,18 @@ class WGANN(object):
 
                 # 1.1: maximize D output on reals === minimize -1*(D(real))
                 # load mnist data
-                #(X_train, y_train) = train_data_gen.__next__()
-                #(X_test, y_test) = validation_data_gen.__next__()
+                (X_train, y_train) = train_data_gen.__next__()
+                (X_test, y_test) = validation_data_gen.__next__()
 
                 # IF TESTING MNIST
-                (X_train, y_train), (X_test, y_test) = mnist.load_data()
+                #(X_train, y_train), (X_test, y_test) = mnist.load_data()
                 # use all available 70k samples
-                X_train = np.concatenate((X_train, X_test))
-                y_train = np.concatenate((y_train, y_test))
-                X_train = np.expand_dims(X_train, axis=3)
+                #X_train = np.concatenate((X_train, X_test))
+                #y_train = np.concatenate((y_train, y_test))
+                #X_train = np.expand_dims(X_train, axis=3)
 
                 # convert to -1..1 range, reshape to (sample_i, 28, 28, 1)
-                X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+                #X_train = (X_train.astype(np.float32) - 127.5) / 127.5
 
                 # draw random samples from real images
                 index = np.random.choice(len(X_train), self.BATCH_SIZE, replace=False)

data_generator.py
@@ -57,7 +57,7 @@ class DataGenerator(object):
       #CHANGED THIS
       X = np.empty((self.batch_size, IM_HEIGHT, IM_WIDTH, 3))
       for i in range(len(list_IDs_temp)):
-        X[i,:,:,:] = get_image(list_IDs_temp[i])/255
+        X[i,:,:,:] = (get_image(list_IDs_temp[i])-127.5)/127.5
         #RESCALE!!!!!!!!!
       return X
 

data_generator_bandw.py
@@ -57,7 +57,7 @@ class DataGenerator(object):
       #CHANGED THIS
       X = np.empty((self.batch_size, IM_HEIGHT, IM_WIDTH, 1))
       for i in range(len(list_IDs_temp)):
-        X[i,:,:,:] = get_image(list_IDs_temp[i])/255
+        X[i,:,:,:] = (get_image(list_IDs_temp[i])-127.5)/255
         #RESCALE!!!!!!!!!
       return X
 