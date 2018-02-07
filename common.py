5e1556dad0938e213086b23a1cb3e207470bd627
Iverus <oliver_kaus@hotmail.de>
2018-02-07 18:57:09 +0000

    Created preprocessing file and added dashboard to common to turn preprocessing functions on and off

--- common.py
+++ common.py
@@ -1,10 +1,33 @@
 #File containing the main parameters used for the neural network
+from enum import Enum
 
-#vgg_net images
+class turned(Enum):
+    ON = 0
+    OFF = 1
+
+'''Model parameters'''
+#Model: vgg_net
 IM_HEIGHT = 100
 IM_WIDTH = 100
+
 '''
+#Model: Inception V3
 IM_HEIGHT = 299
-IM_WIDTH = 299'''
+IM_WIDTH = 299
+'''
+
 NUMBER_CLASSES = 10
 NUMBER_EPOCHS = 50
+
+'''Preprocessing parameters'''
+rotation = turned.ON
+rotation_degrees = 30
+
+zoom = turned.ON
+zoom_horizontal = 0.8
+zoom_vertical = 0.8
+
+horizontal_flip = turned.ON
+vertical_flip = turned.OFF
+
+
--- preprocessing.py
+++ preprocessing.py
@@ -1,129 +1,25 @@
-import os
+from rotation_zoom_flip import *
+from common import *
 from folder_manipulation import dstack_folder
-import numpy as np
-import scipy.ndimage as ndi
+import os
 import cv2
 
-#function which preproccesses the data; can either be one image or multiple images
-#function always returns 4D matrix for multiple pictures and 3D matrix for single picture
-def preprocess_images(pictures_tensor):
-    #second parameter changes degrees of rotation
-    pictures_tensor = random_rotation(pictures_tensor, 30, 0, 1, 2)
-    #second parameters requires touple and determines zoom factor
-    pictures_tensor = random_zoom(pictures_tensor, (0.9, 0.8), 0, 1, 2)
-    pictures_tensor = flip_horizontal(pictures_tensor)
-    pictures_tensor = flip_vertical(pictures_tensor)
-    return pictures_tensor
+def preprocess_images(picture_batch):
+        if rotation == turned.ON:
+            picture_batch = random_rotation(picture_batch, rotation_degrees, 0, 1, 2)
+        if zoom == turned.ON:
+            picture_batch = random_zoom(picture_batch, (zoom_horizontal, zoom_vertical), 0, 1, 2)
+        if horizontal_flip == turned.ON:
+            picture_batch = flip_horizontal(picture_batch)
+        if vertical_flip == turned.ON:
+            picture_batch = flip_vertical(picture_batch)
+        return picture_batch
 
-def random_rotation(x, rg, row_axis=1, col_axis=2, channel_axis=0, fill_mode='nearest', cval=0.):
-    print(x.shape)
-
-    theta = np.deg2rad(np.random.uniform(-rg, rg))
-    rotation_matrix = np.array([[np.cos(theta), -np.sin(theta), 0],
-                                [np.sin(theta), np.cos(theta), 0],
-                                [0, 0, 1]])
-
-    h, w = x.shape[row_axis], x.shape[col_axis]
-    transform_matrix = transform_matrix_offset_center(rotation_matrix, h, w)
-
-    #applying rotation setting to picture(s)
-    transformed_tensors = []
-    for i in range(x.shape[0]):
-        single_tensor = apply_transform(x[i], transform_matrix, channel_axis, fill_mode, cval)
-        transformed_tensors.append(single_tensor)
-
-    transformed_tensors = np.array(transformed_tensors)
-
-    return transformed_tensors
-
-def random_zoom(x, zoom_range, row_axis=1, col_axis=2, channel_axis=0,fill_mode='nearest', cval=0.):
-    print(x.shape)
-    if len(zoom_range) != 2:
-        raise ValueError('`zoom_range` should be a tuple or list of two floats. ''Received arg: ', zoom_range)
-
-    if zoom_range[0] == 1 and zoom_range[1] == 1:
-        zx, zy = 1, 1
-
-    else:
-        zx, zy = np.random.uniform(zoom_range[0], zoom_range[1], 2)
-        zoom_matrix = np.array([[zx, 0, 0],
-                                [0, zy, 0],
-                                [0, 0, 1]])
-
-        transformed_tensors = []
-        h, w = x[0].shape[row_axis], x[0].shape[col_axis]
-        transform_matrix = transform_matrix_offset_center(zoom_matrix, h, w)
-            
-        for i in range(x.shape[0]):
-            single_tensor = apply_transform(x[i], transform_matrix, channel_axis, fill_mode, cval)
-            transformed_tensors.append(single_tensor)
-
-        transformed_tensors = np.array(transformed_tensors)
-                
-        return transformed_tensors
-
-def flip_horizontal(x):
-    print(x.shape)
-    transformed_tensors = []
-    for i in range(x.shape[0]):
-        single_tensor = flip_axis(x[i], 1)
-        transformed_tensors.append(single_tensor)
-
-    transformed_tensors = np.array(transformed_tensors)
-    return transformed_tensors
-
-def flip_vertical(x):
-    print(x.shape)
-    transformed_tensors = []
-    for i in range(x.shape[0]):
-        single_tensor = flip_axis(x[i], 0)
-        transformed_tensors.append(single_tensor)
-
-    transformed_tensors = np.array(transformed_tensors)
-    return transformed_tensors
-
-'''Helper functions '''
-def transform_matrix_offset_center(matrix, x, y):
-    o_x = float(x) / 2 + 0.5
-    o_y = float(y) / 2 + 0.5
-    offset_matrix = np.array([[1, 0, o_x], [0, 1, o_y], [0, 0, 1]])
-    reset_matrix = np.array([[1, 0, -o_x], [0, 1, -o_y], [0, 0, 1]])
-    transform_matrix = np.dot(np.dot(offset_matrix, matrix), reset_matrix)
-    return transform_matrix
-    
-def apply_transform(x, transform_matrix, channel_index=0, fill_mode='nearest', cval=0.):
-    x = np.rollaxis(x, channel_index, 0)
-    final_affine_matrix = transform_matrix[:2, :2]
-    final_offset = transform_matrix[:2, 2]
-    channel_images = [ndi.interpolation.affine_transform(x_channel, final_affine_matrix, final_offset, order=0, mode=fill_mode, cval=cval) for x_channel in x]
-    x = np.stack(channel_images, axis=0)
-    x = np.rollaxis(x, 0, channel_index+1)
-    return x
-
-def flip_axis(x, axis):
-    x = np.asarray(x).swapaxes(axis, 0)
-    x = x[::-1, ...]
-    x = x.swapaxes(0, axis)
-    return x
-
-
-#TESTING CODE
-#get batch in x
+'''Testing the preprocessing function'''
 x = dstack_folder(os.path.join("validation_data","5055540026268","VID36"))
-
-#create test example with 1x100x100x3 dimensions
 y = x[0]
 y = np.expand_dims(y, axis=0)
+z = preprocess_images(y)
 
-y = preprocess_images(y)
-
-'''print ("#Bottom Part#")
-print ("Dimensions of x:")
-print (x.shape)
-
-print ("Dimensions of y:")
-print (y.shape)
-'''
-
-cv2.imwrite("Original.jpeg", x[0])
-cv2.imwrite("Rotated.jpeg", y[0])
+cv2.imwrite("Original.jpeg", y[0])
+cv2.imwrite("Preprocessed.jpeg", z[0])
