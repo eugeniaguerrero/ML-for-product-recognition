b484bb3920204e0f9be249c29f88fa4d77ddf0ef
Arun Kothari <arun.kothari17@imperial.ac.uk>
2018-02-21 15:23:27 +0000

    changing file paths

--- src/PREPROCESSING/frame_differencing_folders.py
+++ src/PREPROCESSING/frame_differencing_folders.py
@@ -1,12 +1,13 @@
-from GroupProject.src.DATA_PREPARATION.folder_manipulation import *
-import shutil
-import cv2
-import numpy as np
-import os
+import inspect, sys, os, shutil, time, cv2, numpy as np
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)
+sys.path.insert(0,parentdir)
 from skimage.measure import compare_ssim
-from GroupProject.src.PREPROCESSING.histogram_equalisation import *
-import time
 from skimage.util.shape import view_as_blocks
+from src.DATA_PREPARATION.folder_manipulation import *
+from src.PREPROCESSING.histogram_equalisation import *
+
 
 def mse(imageA, imageB):
     # sum of the squared difference between the two images;
@@ -16,7 +17,7 @@ def mse(imageA, imageB):
     return err
 
 def square_border(img):
-    if  img.shape[0] > img.shape[1]:
+    if img.shape[0] > img.shape[1]:
         border_size = int((img.shape[0] - img.shape[1])/2)
         bordered_img = cv2.copyMakeBorder(img, top=0, bottom=0, left=border_size, right=border_size,
                                     borderType=cv2.BORDER_CONSTANT, value=[0, 0, 0])
@@ -27,102 +28,149 @@ def square_border(img):
 
     return bordered_img
 
-def kernel_compare(individual_product, next_product):
+def img_exception(img):
+    #return False
+    height = img.shape[0]
+    width = img.shape[1]
 
+    if (height == RAW_HEIGHT) and (width == RAW_WIDTH):
+        return True
+
+    if (height < IM_HEIGHT) or (width < IM_WIDTH):
+        return True
+
+    return False
+
+
+def kernel_compare(individual_product, next_product):
     region_size = 64
-    im_h = 1024
-    im_w = 1280
+
+    im_h = individual_product.shape[0]
+    im_w = individual_product.shape[1]
+
     bl_h, bl_w = region_size, region_size
 
-    # 1 means the pics are identical. Try 85
+    # 1 means the pics are identical. Try 085
     ssim_threshold = 0.85
-    # 0 means the images are identical. Try 25
-    # mse_threshold = 30
+
+    white = 255
 
     gray_img = np.zeros((im_h, im_w))
-    # include this if we want to start with a white square
-    gray_img.fill(255)
+    gray_img.fill(white)
+    gray_img = gray_img.astype(np.uint8)
+
+    color_img = np.zeros((im_h, im_w, 3))
+    color_img.fill(white)
+    color_img = color_img.astype(np.uint8)
 
     grayA = cv2.cvtColor(individual_product, cv2.COLOR_BGR2GRAY)
     grayB = cv2.cvtColor(next_product, cv2.COLOR_BGR2GRAY)
 
-    # for row in np.arange(im_h - bl_h + 1, step=bl_h):
-    #     for col in np.arange(im_w - bl_w + 1, step=bl_w):
+    for row in np.arange(im_h - bl_h + 1, step=bl_h):
+        for col in np.arange(im_w - bl_w + 1, step=bl_w):
+
+            grayA_kernel = grayA[row:row + bl_h, col:col + bl_w]
+            grayB_kernel = grayB[row:row + bl_h, col:col + bl_w]
+
+            (ssim_score, diff) = compare_ssim(grayA_kernel, grayB_kernel, full=True)
+            # checking to see if the pictures are different
+            if (ssim_score < ssim_threshold):
+                gray_img[row:row + bl_h, col:col + bl_w] = grayA[row:row + bl_h, col:col + bl_w]
+                color_img[row:row + bl_h, col:col + bl_w] = individual_product[row:row + bl_h, col:col + bl_w]
+
+    thresh = 254.9
+    threshold_img = cv2.threshold(gray_img, thresh, white, cv2.THRESH_BINARY_INV)[1]
+
+    # grayA_blocks = view_as_blocks(grayA, (region_size, region_size))
+    # grayB_blocks = view_as_blocks(grayB, (region_size, region_size))
     #
-    #         grayA_kernel = grayA[row:row + bl_h, col:col + bl_w]
-    #         grayB_kernel = grayB[row:row + bl_h, col:col + bl_w]
+    # grayA_blocks_reshaped = np.reshape(grayA_blocks, (-1, region_size, region_size))
+    # grayB_blocks_reshaped = np.reshape(grayB_blocks, (-1, region_size, region_size))
     #
-    #         (ssim_score, diff) = compare_ssim(grayA_kernel, grayB_kernel, full=True)
+    # ssim_array = map(lambda v1, v2: compare_ssim(v1, v2), grayA_blocks_reshaped, grayB_blocks_reshaped)
     #
-    #         #mse_score = mse(grayA_kernel, grayB_kernel)
+    # #ssim_array = np.fromiter(ssim_array, dtype=np.uint8)
+    # ssim_array = np.array(list(ssim_array))
     #
-    #         # checking to see if the pictures are different
-    #         if (ssim_score < ssim_threshold): #(mse_score > mse_threshold): #
-    #             gray_img[row:row + bl_h, col:col + bl_w] = grayA[row:row + bl_h, col:col + bl_w]
+    # ssim_array_reshape = np.reshape(ssim_array, (int(im_h / region_size), int(im_w / region_size)))
+    #
+    # ssim_array_reshape = np.where(ssim_array_reshape < ssim_threshold, 1, 0)
+    # ssim_array_full_size = np.kron(ssim_array_reshape, np.ones((region_size, region_size)))
+    #
+    # gray_img = np.multiply(ssim_array_full_size, grayA)
+    #
+    # thresh = 0
+    # color = 255
+    # threshold_img = cv2.threshold(gray_img, thresh, color, cv2.THRESH_BINARY)[1]
+    #
+    # threshold_img = threshold_img.astype(np.uint8)
 
-    grayA_blocks = view_as_blocks(grayA, (region_size, region_size))
-    grayB_blocks = view_as_blocks(grayB, (region_size, region_size))
-
-    grayA_blocks_reshaped = np.reshape(grayA_blocks, (-1, region_size, region_size))
-    grayB_blocks_reshaped = np.reshape(grayB_blocks, (-1, region_size, region_size))
-
-    ssim_array = map(lambda v1, v2: compare_ssim(v1, v2), grayA_blocks_reshaped, grayB_blocks_reshaped)
-
-    # ssim_array = np.fromiter(ssim_array, dtype=np.uint8)
-    ssim_array = np.array(list(ssim_array))
-
-    ssim_array_reshape = np.reshape(ssim_array, (int(im_h / region_size), int(im_w / region_size)))
-    ssim_array_reshape = np.where(ssim_array_reshape < ssim_threshold, 1, 0)
-    ssim_array_full_size = np.kron(ssim_array_reshape, np.ones((region_size, region_size)))
-
-    gray_img = np.multiply(ssim_array_full_size, grayA)
-
-    gray_img = gray_img.astype(np.uint8)
-
-    # thresh = 254.99
-    # gray_img = cv2.threshold(gray_img, thresh, 255, cv2.THRESH_BINARY_INV)[1]
-
-    thresh = 0
-    color = 255
-    gray_img = cv2.threshold(gray_img, thresh, color, cv2.THRESH_BINARY)[1]
-
-    _, contours, _ = cv2.findContours(gray_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, _ = cv2.findContours(threshold_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # if we have no contours then there is no difference between frames
     if contours == []:
         return individual_product
 
     largest_area = sorted(contours, key=cv2.contourArea)[-1]
+    # print("Largest:", cv2.contourArea(largest_area))
+    # next_largest_area = sorted(contours, key=cv2.contourArea)[-2]
+    # print("2nd Largest:", cv2.contourArea(next_largest_area))
 
     x, y, w, h = cv2.boundingRect(largest_area)
 
-    individual_product = individual_product[y: y + h, x: x + w]
+    cropped_individual_product = individual_product[y: y + h, x: x + w]
 
-    cropped_block_img = CLAHE_equalisation(individual_product)
+    histogram_block_img = CLAHE_equalisation(cropped_individual_product)
 
-    bordered_img = square_border(cropped_block_img)
+    # blank_square = np.zeros((h, w))
+    # blank_square.fill(0)
+    #
+    # histogram_grey_block_img = cv2.cvtColor(histogram_block_img, cv2.COLOR_BGR2GRAY)
+    #
+    # kernel = np.ones((5, 5), np.uint8)
+    # # morphology removes noise from the picture
+    # close_operated_image = cv2.morphologyEx(histogram_grey_block_img, cv2.MORPH_CLOSE, kernel)
+    #
+    # # OTSU method finds optimal threshold
+    # _, thresholded = cv2.threshold(close_operated_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    #
+    # # reduces noise by taking the median value in the kernel
+    # median = cv2.medianBlur(thresholded, 5)
+    #
+    # _, contours, _ = cv2.findContours(median, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # blank_square = cv2.drawContours(blank_square, contours, -1, (100, 0, 255), 2)
+    #
+    # cv2.imshow("mypic", cropped_block_img)
+    # cv2.waitKey(0)
 
-    return bordered_img
+    # move to after we look for exceptions
+
+    return histogram_block_img
 
 
-def frame_difference_collection(source, destination):
+def frame_difference_collection(source, destination, exceptions):
     i = 0
     num_of_pics = 0
+
+    # find the number of pictures in the video set
     for filename in sorted(os.listdir(source)):
         if filename.endswith('.jpg') or filename.endswith('.png'):
             num_of_pics = num_of_pics + 1
 
-    pictures = np.empty((num_of_pics, 1024, 1280, 3), dtype=np.uint8)
+    # store the pictures in an array
+    pictures = np.empty((num_of_pics, RAW_HEIGHT, RAW_WIDTH, 3), dtype=np.uint8)
     for filename in sorted(os.listdir(source)):
         if filename.endswith('.jpg') or filename.endswith('.png'):
             filepath = os.path.join(source, filename)
             image = cv2.imread(filepath)
-            pictures[i, ...] = image
-            i += 1
+            # pictures[i, ...] = image
+            if image.shape[0] == RAW_HEIGHT and image.shape[1] == RAW_HEIGHT:
+                pictures[i] = image
+                i += 1
 
     index = 0
-
     for _ in pictures:
+
         if index == len(pictures)-1:
             individual_product = pictures[index]
             next_product = pictures[index-1]
@@ -132,44 +180,91 @@ def frame_difference_collection(source, destination):
 
         new_img = kernel_compare(individual_product, next_product)
 
-        file_name = str(index) + '_' + filename
-        original_file_name = str(index) + '_original_' + filename
+        # here we check whether frame differencing has detected movement
+        if img_exception(new_img):
+            file_name = 'exception' + str(index) + '_' + filename
+            cv2.imwrite(os.path.join(exceptions, file_name), individual_product)
+        else:
+            new_img = square_border(new_img)
+            # note that we still store the remaining video collection
+            file_name = str(index) + '_' + filename
+            original_file_name = str(index) + '_original_' + filename
 
-        cv2.imwrite(os.path.join(destination, file_name), new_img)
-        cv2.imwrite(os.path.join(destination, original_file_name), individual_product)
-
+            cv2.imwrite(os.path.join(destination, file_name), new_img)
+            # include for comparison
+            cv2.imwrite(os.path.join(destination, original_file_name), individual_product)
         index += 1
 
-# The directory where the augmented images are going to be saved
-difference = 'frame_difference_sample_data'
 
-# The directory where the "to-be-edited" images are already saved
-# topdir = 'sample_data'
+def main_diff(folder_set):
+    my_count = 0
 
-topdir = 'training_data'
+    dir = os.getcwd()
+    src_dir = os.path.dirname(dir)
+    Group_dir = os.path.dirname(src_dir)
+    data_folder = 'DATA'
 
-dir = os.getcwd()
-print(dir)
-exit()
+    # ensure that folder exist
+    for folder in folder_set:
+        my_folder = os.path.join(Group_dir, data_folder, folder)
+        print("my_folder", my_folder)
+        # if not(os.path.exists(my_folder)):
+        #     print(folder, " does not exist")
+        #     exit(1)
+        assert (os.path.exists(my_folder)), " folder does not exist"
 
-if os.path.exists(difference):
-    shutil.rmtree(difference)
-os.makedirs(difference)
+    # loop on training, validation and testing
+    for folder in folder_set:
+        difference_set = 'FD_' + folder
+        my_folder = os.path.join(Group_dir, data_folder, difference_set)
+        if os.path.exists(my_folder):
+            shutil.rmtree(my_folder)
+        os.makedirs(my_folder)
 
-top_folders = get_folders(topdir)
+        data_set = os.path.join(Group_dir, data_folder, folder)
+        class_folders = get_folders(data_set)
 
-for folder in top_folders:
-    class_folder = os.path.join(dir,topdir, folder)
-    diff_class_folder = os.path.join(dir, difference, folder)
-    os.makedirs(diff_class_folder)
-    product_video = get_folders(class_folder)
+        exceptions = 'Exceptions_' + folder
+        my_exceptions = os.path.join(Group_dir, data_folder, exceptions)
 
-    for video in product_video:
-        video_folder = os.path.join(class_folder, video)
-        diff_video_folder = os.path.join(diff_class_folder, video)
-        os.makedirs(diff_video_folder)
-        image_set = get_image_names(video_folder)
-        start = time.time()
-        frame_difference_collection(video_folder, diff_video_folder)
-        end = time.time()
-        print("Time", end - start)
+        if os.path.exists(my_exceptions):
+            shutil.rmtree(my_exceptions)
+        os.makedirs(my_exceptions)
+
+
+        # loop on class folders
+        for class_set in class_folders:
+            class_folder = os.path.join(data_set, class_set)
+
+            diff_class_folder = os.path.join(Group_dir, data_folder, difference_set, class_set)
+            os.makedirs(diff_class_folder)
+
+            exceptions_class_folder = os.path.join(Group_dir, data_folder, exceptions, class_set)
+            os.makedirs(exceptions_class_folder)
+
+            product_video_set = get_folders(class_folder)
+
+            # loop on video sets
+            for video in product_video_set:
+                video_folder = os.path.join(class_folder, video)
+                diff_video_folder = os.path.join(diff_class_folder, video)
+                os.makedirs(diff_video_folder)
+                frame_difference_collection(video_folder, diff_video_folder, exceptions_class_folder)
+                my_count = my_count + 1
+                # avg_time = (time.time() - start_run)/my_count
+                # print("Avg time per set:", avg_time)
+
+if __name__ == "__main__":
+    start_run = time.time()
+    print("Start Run")
+    # The directory where the augmented images are going to be saved
+    # my_folders = ['training_data', 'validation_data', 'test_data']
+    my_folders = ['sample1']
+
+    main_diff(my_folders)
+
+    end_run = time.time()
+    print("Total Run Time (mins): ", (end_run - start_run)/60)
+
+
+
