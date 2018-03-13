b0fa3ea61a1db11022ecb6bc99b3e4c2f1c0c899
peterhedley <hedleyplh@gmail.com>
2018-03-13 18:47:41 +0000

    image Test

--- Master.py
+++ Master.py
@@ -1,12 +1,13 @@
-from src.NN_MODELS.cnn_lstm import *
+#from src.NN_MODELS.cnn_lstm import *
 from src.NN_MODELS.inception_v3 import *
 from src.NN_MODELS.vgg_testing import *
 #from src.NN_MODELS. import *
 from src.common import *
 #from src.NN_MODELS.network_tests import *
-from src.DATA_PREPARATION.partition_grouped import *
-from src.DATA_PREPARATION.partition_grouped_folders import *
+#from src.DATA_PREPARATION.partition_grouped import *
+#from src.DATA_PREPARATION.partition_grouped_folders import *
 import keras
+import time
 
 #TEST DATA GROUPED
 #sort_data(SOURCE,TRAIN_DATA,TEST_DATA,VALIDATE_DATA)
@@ -31,16 +32,34 @@ channel_shift_range=0.2,
 shear_range=0.35,
 zoom_range = [0.7, 1.3])'''
 
-'''
+datagen = ImageDataGenerator(
+    rotation_range=10,
+    horizontal_flip=True,
+    vertical_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    channel_shift_range=0.1,
+    shear_range=0.1,
+    rescale=1. / 255)
+
 datagenval = ImageDataGenerator(
     rescale=1. / 255)
 
-for image_size in [300]:
+file = open('MODEL_OUTPUTS/image_sizetime.txt',mode='w')
+file.write("Image_Size,Start,End,Difference\n")
+
+#,80,100,150,200,250,300,350,400,500
+for image_size in [50,80,100,150,200,250,300,400,500]:
+    start = time.time()
     IM_HEIGHT = image_size
     IM_WIDTH = image_size
     vgg_ = VGG(False)
     vgg_.train(train_directory_=TRAIN_DATA, validation_directory_=VALIDATE_DATA, model_description= "image_test_size" + str(image_size), epochs=NUMBER_EPOCHS,datagen=datagen,datagenval=datagenval)
-'''
+    end = time.time()
+    diff = end-start
+    file.write(str(image_size) + ',' + str(start) + ',' + str(end) + ',' + str(diff) + '\n')
+
+file.close()
 
 '''
 inception_v3_ = INCEPTION_V3()
--- src/common.py
+++ src/common.py
@@ -14,10 +14,10 @@ IM_WIDTH = 299'''
 PRETRAINED_MODEL = False
 NUMBER_CLASSES = 10
 BATCH_SIZE = 16
-NUMBER_EPOCHS = 20
+NUMBER_EPOCHS = 1
 NUMBER_CHANNELS = 3
 IMAGES_PER_FOLDER = 4
-SEND_TO_SLACK = True
+SEND_TO_SLACK = False
 
 CHECKPOINTS_FOLDER = os.path.join('MODEL_OUTPUTS','checkpoints')
 MODEL_SAVE_FOLDER = os.path.join('MODEL_OUTPUTS','models')
@@ -29,7 +29,7 @@ JSON_LOG_FILE = os.path.join('MODEL_OUTPUTS','loss_log.json')
 JSON_OLD_LOGS_FOLDER = os.path.join('MODEL_OUTPUTS','old_json')
 
 SOURCE = os.path.join("DATA","product-image-dataset3")
-TRAIN_DATA = os.path.join("DATA","fvalidation_data")
+TRAIN_DATA = os.path.join("DATA","ftraining_data")
 VALIDATE_DATA = os.path.join("DATA","fvalidation_data")
 TEST_DATA = os.path.join("DATA","ftest_data")
 DEBUG_FOLDER = os.path.join("DATA","DEBUGGING_DATA","debug_folder")
