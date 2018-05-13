55fb1100cc59b64d29623cfc54efd495bfb57356
Peter Hedley <ph817@gpu03.doc.ic.ac.uk>
2018-05-13 11:45:09 +0100

    Master File Update

--- Master.py
+++ Master.py
@@ -14,7 +14,7 @@ import time
 
 
 
-FD_DATA_LOC = os.path.join("DATA","ambient_data","raw_data")#"1104_raw_full_ambient")#,"1104_FD_Full_Set")
+FD_DATA_LOC = os.path.join("DATA","ambient_data","fd_data")#raw_"1104_raw_full_ambient")#,"1104_FD_Full_Set")
 NORMAL_DATA_LOC =  os.path.join("DATA","ambient_data","raw_data")
 
 
@@ -66,10 +66,10 @@ for FD_ in [1]:
         #STRING TO SAVE MODEL AS
         vals = "LR-" + str(lr) + "_C1-" + str(conv1_size) + "_C2-" + str(conv2_size) + "_DS-" + str(dense_size)
         vals +=  "_D-" + str(decay) + "_M-" + str(moment) + "_IM-" + str(IM_WIDTH) + "_FD-" + FD + "_Time_Stamp-" + str(time.time()) + "_Preprop-" + Preprop
-        inc = INCEPTION_V3();
-        inc.train(train_directory_=TRAIN_DATA, validation_directory_=VALIDATE_DATA, model_description= "small_dataset" + "Preprop-" + Preprop,epochs=NUMBER_EPOCHS,datagen=datagen,datagenval=datagenval)
-        #vgg_ = VGG(False,IM_HEIGHT=IM_HEIGHT,IM_WIDTH=IM_WIDTH,lr=lr,conv1_size=conv1_size,conv2_size=conv2_size,dense_size=dense_size,decay=decay,moment=moment)
-        #vgg_.train(train_directory_=TRAIN_DATA, validation_directory_=VALIDATE_DATA, model_description= "full_dataset_"+ vals,epochs=NUMBER_EPOCHS,datagen=datagen,datagenval=datagenval)
+        #inc = INCEPTION_V3();
+        #inc.train(train_directory_=TRAIN_DATA, validation_directory_=VALIDATE_DATA, model_description= "small_dataset" + "Preprop-" + Preprop,epochs=NUMBER_EPOCHS,datagen=datagen,datagenval=datagenval)
+        vgg_ = VGG(False,IM_HEIGHT=IM_HEIGHT,IM_WIDTH=IM_WIDTH,lr=lr,conv1_size=conv1_size,conv2_size=conv2_size,dense_size=dense_size,decay=decay,moment=moment)
+        vgg_.train(train_directory_=TRAIN_DATA, validation_directory_=VALIDATE_DATA, model_description= "full_dataset_"+ vals,epochs=NUMBER_EPOCHS,datagen=datagen,datagenval=datagenval)
 
 end = time.time()
 diff = end-start
